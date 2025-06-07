afeedb93b5bd8af4ef0f48ba628110fb6ac4b982
MancillaO <edgarmncllav04@gmail.com>
2025-06-06 23:42:58 -0600

    add first structure

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,6 +1,14 @@
+import { UrlModel } from '#models/urlModel.js'
+
 export class UrlController {
   static async shorten (req, res) {
+    const { url, code, description } = req.body
 
+    if (!url) {
+      return res.status(400).json({ error: 'URL is required' })
+    }
+
+    const cleanUrl = url.trim()
   }
   static async redirect (req, res) {
 
